443117e2e8edd98897a4b1d136a3f9f4ec8e7807
chucholo <chuchola20a@gmail.com>
2025-08-07 14:57:13 -0600

    Se agrego cors

---
+++ src/main/java/com/hospital/backendHospital/config/CorsConfig.java
@@ -0,0 +1,26 @@
+package com.hospital.backendHospital.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of("http://localhost:3000", "https://enchanting-growth-production.up.railway.app")); // Aquí el dominio de tu frontend
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true); // solo si usas cookies, sino déjalo en false
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+}
