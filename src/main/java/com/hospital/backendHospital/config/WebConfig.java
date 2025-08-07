719e4215c555a4fd311bf1ef32e343148f0e241e
chucholo <chuchola20a@gmail.com>
2025-08-07 13:50:41 -0600

    Se agrego el cors

---
+++ src/main/java/com/hospital/backendHospital/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.hospital.backendHospital.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Aplica a todas las rutas
+                        .allowedOrigins("*") // Cambia por el dominio frontend si es necesario
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
