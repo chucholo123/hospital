bb3d7f749ccc2aac7bf5e255a5fcac1cec3bcfca
chucholo <chuchola20a@gmail.com>
2025-09-25 22:56:08 -0600

    Fix bug con la entidad

--- src/main/java/com/hospital/backendHospital/models/entity/FileEntity.java
+++ src/main/java/com/hospital/backendHospital/models/entity/FileEntity.java
@@ -17,7 +17,7 @@ import java.util.UUID;
 public class FileEntity {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
     private String name;
